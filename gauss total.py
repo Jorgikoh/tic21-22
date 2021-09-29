c02fe6adc3c5b1c8b13870bafce5b97d9e4ab5cf
Jorgikoh <90762668+Jorgikoh@users.noreply.github.com>
2021-09-29 11:13:53 +0200

    Add files via upload

---
+++ gauss total.py
@@ -0,0 +1,19 @@
+def gausstotal():
+    n=input ("hasta que numero quieres contar: ")
+    suma_par=0
+    suma_impar=0
+    suma_total=0
+    resto=0
+    for cont in range(1,n+1):
+        resto= cont % 2
+        suma_total=suma_total+cont
+        if(resto==0):
+            suma_par=suma_par+cont
+        else:
+            suma_impar=suma_impar+cont
+        print(str(cont) +" pares= "+str(suma_par)+" impares= "+str(suma_impar))
+    print("la suma de los pares= "+ str(suma_par))
+    print("la suma de los impares= "+str(suma_impar))
+    print("la suma total= "+str(suma_total))
+
+gausstotal()
