6b81a7eacb571f30a7d31dfabb8d6c0ed09852b9
Jorgikoh <90762668+Jorgikoh@users.noreply.github.com>
2022-02-23 11:54:51 +0100

    Update arrayse5.cpp

--- arrayse5.cpp
+++ arrayse5.cpp
@@ -5,14 +5,14 @@ int main(){
 	int cont;
 	
 	
-	}
+	
 	//leo datos 
 	for(cont=0;cont<5;cont++){
 		//num=input("dame un numero: ")//
 		printf("introduce la letra %d : ",cont);
 		scanf(" %c",&letras[cont]);
 	}
-	//escribo la palabra al revés 
+	//escribo la palabra al revÃ©s 
 	for(cont=4;cont>=0;cont--){
 		printf("%c",letras[cont]);
 	}
